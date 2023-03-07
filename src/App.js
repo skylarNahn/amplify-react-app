a721a787eb9ffdbb7528a0700c571952224192a2
skylarNahn <nahn@madisoncollege.edu>
2023-03-07 11:41:57 -0600

    adding usestate and ability to specify limit and start

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ import { API } from 'aws-amplify'
 import './App.css';
 
 const App = () => {
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([])
 
   // Define function to all API
-  async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins')
+  const fetchCoins = async() => {
+    const { limit, start } = input
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
     updateCoins(data.coins)
-  }
+  };
 
   // Call fetchCoins function when component loads
   useEffect(() => {
     fetchCoins()
-  }, [])
+  }, []);
+
 
   return (
     <div className="App">
