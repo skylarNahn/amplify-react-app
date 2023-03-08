00276e9884e5104a30cf2bca0a7d3a048fdb5a16
skylarNahn <nahn@madisoncollege.edu>
2023-03-08 17:05:28 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import useState and useEffect hooks from React
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'  
 
 // Import the API category from AWS Amplify
 import { API } from 'aws-amplify'
